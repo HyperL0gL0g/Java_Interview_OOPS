e3064f65107a871c2954f61ee1bc44d5c46cff17
logicinfinite <1705099@kiit.ac.in>
2021-02-06 00:36:48 +0530

    oops

---
+++ lol.java
@@ -0,0 +1,23 @@
+//this java class is only for practice sessions
+
+public class lol extends Java {
+    public static void main(String[] args) {
+      lol o = new lol();
+      o.skill();
+
+    }
+}
+class Java
+{
+    public void skill()
+    {
+        System.out.println("lol");
+    }
+}
+class J
+{
+    public void skill()
+    {
+        System.out.println("lol");
+    }
+}
